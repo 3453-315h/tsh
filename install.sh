40a832dd961d5c88937aaad1eb52591c7e4edca0
Simone Baracchi <simonebaracchi>
2018-03-11 19:43:43 +0100

    Add installer for systemd

--- install.sh
+++ install.sh
@@ -1,38 +1,54 @@
 #!/bin/bash
-echo "Downloading & Installing Packages..."
+printf "Downloading & Installing Packages...\n"
 apt-get install -y python nmap dnsutils mtr python-pip && pip install telepot
 
-echo
-echo
-echo --------------------------------
-echo
-echo
+printf "\n\n--------------------------------\n\n"
 echo "Enter your Telegram BOT Token. "
 echo "Telegram BOT Token can be asked to BotFather. "
 read -r TG_BOT_TOKEN
 
 echo "bot_token = '$TG_BOT_TOKEN'" > tempconfig.py
 
-echo
-echo
-echo --------------------------------
-echo
-echo
+printf "\n\n--------------------------------\n\n"
 echo "Trying to find out your Telegram sender-id..."
 python get-sender-id.py  | grep "'id'" | uniq -c | awk '{ print $3 }' | sed s'/,//'
-
 rm tempconfig.py
 
 echo "Enter your Telegram Sender ID. "
-echo "Telegram Sender ID is your identifier. If automatic sender ID retrieval has failed, try sending a private message to your bot in Telegram, and try again. "
+echo "Telegram Sender ID is your identifier. Only this user will be enabled to send commands to this bot. If automatic sender ID retrieval has failed, try sending a private message to your bot in Telegram, and try again. "
 read -r SENDER_ID
 
+cp config.example.py config.py
 sed -i s"/MY-TG-BOT-TOKEN/$TG_BOT_TOKEN/" config.py
 sed -i s"/MY-SENDER-ID-LIST/$SENDER_ID/" config.py
 
-echo "Configure supervisor? (y/n)"
+printf "\n\n--------------------------------\n\n"
+echo " Select an option"
+echo "[0] Exit installer"
+echo "[1] Configure daemon with systemctl (for systemd-enabled distros)"
+echo "[2] Disable daemon with systemctl"
+echo "[3] Configure daemon with supervisor (for supervisor-enabled distros)"
 read -r SUPERVISOR
-if [ $SUPERVISOR == "y" ]; then
+case $SUPERVISOR in
+0)
+	exit 0
+	;;
+1)
+	cp systemctl/tsh.example.service /tmp/tsh.service
+	DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+	sed -i s"#MY-PATH#$DIR#" /tmp/tsh.service
+	mv /tmp/tsh.service /etc/systemd/system/multi-user.target.wants/tsh.service
+	systemctl daemon-reload
+	systemctl restart tsh
+	;;
+2)
+	systemctl stop tsh
+	systemctl disable tsh
+	rm /etc/systemd/system/multi-user.target.wants/tsh.service
+	systemctl daemon-reload
+	systemctl reset-failed
+	;;
+3)
 	apt-get install -y supervisor
 
 	echo "Configuring tsh as a service..."
@@ -43,4 +59,9 @@ if [ $SUPERVISOR == "y" ]; then
 
 	echo "Starting tsh service..."
 	supervisorctl start tsh
-fi
+	;;
+*)
+	echo Unrecognized option $SUPERVISOR, exiting
+	exit 1
+	;;
+esac
--- tsh.py
+++ tsh.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # Author: Sami Yessou - samiii@protonmail.com
 # Telegram Remote-Shell
 # Control your Linux System remotely via Telegram API
@@ -27,43 +27,47 @@ def handle(msg):
             output=os.popen("ping -c1 "+host).read()
             bot.sendMessage(chat_id, output)
 
-      if command == '/mtr':
+      elif command == '/mtr':
             host = str(args[1])
             output=os.popen("mtr --report "+host).read()
             bot.sendMessage(chat_id, output)
 
-      if command == '/nmap':
+      elif command == '/nmap':
             value = str(args[1])
             host = str(args[2])
             output=os.popen("nmap -A "+value+" "+host).read()
             bot.sendMessage(chat_id, output)
 
-      if command == '/curl':
+      elif command == '/curl':
             host = str(args[1])
             output=os.popen("curl -Iv "+host).read()
             bot.sendMessage(chat_id, output)
 
-      if command == '/dig':
+      elif command == '/dig':
             type = str(args[1])
             host = str(args[2])
             output=os.popen("dig +short "+type+" "+host).read()
             bot.sendMessage(chat_id, output)
 
-      if command == '/whois':
+      elif command == '/whois':
             host = str(args[1])
             output=os.popen("whois "+host).read()
             bot.sendMessage(chat_id, output)
 
 
-      if command == '/sysinfo':
+      elif command == '/sysinfo':
             output=os.popen("df -h && free -m && netstat -tunlp").read()
             bot.sendMessage(chat_id, output)
 
 
-      if command == '/sh':
+      elif command == '/sh':
             cmd = str(args[1])
             output=os.popen(cmd).read()
             bot.sendMessage(chat_id, output)
+
+      else:
+            bot.sendMessage(chat_id, 'Sorry, this does not seem to be a valid command.')
+
 
 bot = telepot.Bot(config.bot_token)
 bot.message_loop(handle)
