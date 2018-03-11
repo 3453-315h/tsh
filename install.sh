137c73039e83b4e4e30faaf195e24feacd23c350
Simone Baracchi <simonebaracchi>
2018-03-11 18:54:53 +0100

    Simplify installation

--- install.sh
+++ install.sh
@@ -2,17 +2,29 @@
 echo "Downloading & Installing Packages..."
 apt-get install -y python nmap dnsutils mtr python-pip && pip install telepot
 
-echo "Enter your Telegram BOT Token: "
+echo
+echo
+echo --------------------------------
+echo
+echo
+echo "Enter your Telegram BOT Token. "
+echo "Telegram BOT Token can be asked to BotFather. "
 read -r TG_BOT_TOKEN
 
 echo "bot_token = '$TG_BOT_TOKEN'" > tempconfig.py
 
+echo
+echo
+echo --------------------------------
+echo
+echo
 echo "Trying to find out your Telegram sender-id..."
 python get-sender-id.py  | grep "'id'" | uniq -c | awk '{ print $3 }' | sed s'/,//'
 
 rm tempconfig.py
 
-echo "Enter your Telegram Sender ID: "
+echo "Enter your Telegram Sender ID. "
+echo "Telegram Sender ID is your identifier. If automatic sender ID retrieval has failed, try sending a private message to your bot in Telegram, and try again. "
 read -r SENDER_ID
 
 sed -i s"/MY-TG-BOT-TOKEN/$TG_BOT_TOKEN/" config.py
